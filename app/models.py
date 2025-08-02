8978fd42b1b0cd5e122b94294b0e1b26578c4ec2
1Gift3 <joshuaramaoka@gmail.com>
2025-08-02 16:56:54 +0200

    I was rebuilding init, installing missing packages, fixing relative imports, getting thee app ready, registering and logging in using postman, hooking up postgreand running your migrations

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ from datetime import datetime
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)  # store hashed passwords!
     
     def __repr__(self):
