4fe444f3e0f89fb70b22a5931573cd3e59596425
1Gift3 <joshuaramaoka@gmail.com>
2025-08-12 16:47:28 +0200

    Configuring app fact, resolving import issues,testing setups environments, env config adjustments, and veryfying run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,22 @@
 from flask import Flask
-from .extensions import db, migrate, jwt
+from flask_migrate import Migrate
+from .extensions import db, jwt
 from .routes.auth_routes import auth_bp
 from .routes.posts_routes import posts_bp
 from .routes.main_routes import main_bp
+from config import Config  # Import your config
 
+migrate = Migrate()  # Ensure migrate is defined here
 
-
-
-def create_app(test_config=None):
+def create_app(config_class=None):
     app = Flask(__name__)
-    app.config.from_object('config.Config')
-
-    if test_config:
-        app.config.update(test_config)
-
-    app.config["DEBUG"] = True
-
+    
+    if config_class is None:
+        from config import DevelopmentConfig
+        app.config.from_object(DevelopmentConfig)
+    else:
+        app.config.from_object(config_class)
+    
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
